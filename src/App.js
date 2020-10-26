22afb9cb5fd6edb0a2b7481948377b66c36afed9
Alberto Medellin <albertomedellin85@gmail.com>
2020-10-26 14:02:22 -0700

    Added state, working on style

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import CardList from './components/CardList'
 import SearchBox from './components/SearchBox';
 import {robots} from './robots'
- 
-class App extends React.Component {
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Robo Friends</h1>
-        <SearchBox/>
-        <CardList robots={robots}/>
-      </div>
-    );
-  }
+
+
+function App(){
+
+  const [robotsInfo, setRobots] = useState(robots);
+  const [searchInput, setSearchInput] = useState('');
+
+  return (
+    <div className="App">
+      <h1 className='f3 f2-m f1-l fw2 black-90 mv3'>Robo Friends</h1>
+      <SearchBox searchInput={searchInput} setSearchInput={setSearchInput} setRobots={setRobots} robotsInfo={robotsInfo}/>
+      <CardList robots={robotsInfo} />
+    </div>
+  );
+
 }
+
+
+
  
 export default App;
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import '../styles/searchBox.css'
+import '../styles/searchBox.css';
+import {robots} from '../robots'
 
-function SearchBox() {
+function SearchBox({ setRobots, searchInput, setSearchInput}) {
+
+    const handleChange=(event)=>{
+        event.preventDefault();
+        setSearchInput(event.target.value)
+        const filteredRobots = robots.filter( robot => {
+            return robot.name.toLowerCase().includes(searchInput.toLowerCase())
+        });
+        setRobots(filteredRobots)
+        
+    }
+
     return (
         <div className='search_container'>
             <h1>Search Box</h1>
             <input 
+            onChange={(e)=>{
+                handleChange(e)
+            }}
             className='pa3 ba b--green bg-lightest-blue'
             type='search' 
             placeholder='search robots' />
