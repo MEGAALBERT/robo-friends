4a666a19141b8638055103edda9039ca3d83f656
Alberto Medellin <albertomedellin85@gmail.com>
2020-10-26 22:16:37 -0700

    finished gallery

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect } from 'react';
 import './App.css';
 import CardList from './components/CardList'
+import Scroll from './components/Scroll';
 import SearchBox from './components/SearchBox';
 import {robots} from './robots';
 
@@ -11,11 +12,19 @@ function App(){
   const [robotsInfo, setRobots] = useState(robots);
   const [searchInput, setSearchInput] = useState('');
 
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response=> response.json())
+    .then(users => setRobots(users))
+  }, [])
+
   return (
     <div className="App">
       <h1 className='f3 f2-m f1-l fw2 black-90 mv3' style={{color:'violet'}}>Robo Friends</h1>
       <SearchBox searchInput={searchInput} setSearchInput={setSearchInput} setRobots={setRobots} robotsInfo={robotsInfo}/>
+      <Scroll>
       <CardList robots={robotsInfo} />
+      </Scroll>
     </div>
   );
 
